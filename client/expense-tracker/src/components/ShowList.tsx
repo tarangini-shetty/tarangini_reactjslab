cba1d799228e3ef45945fa954484af2271f03ce8
tarangini-shetty <tarangini.shetty@gmail.com>
2022-11-01 19:43:07 +0530

    lab solutions

--- client/expense-tracker/src/components/ShowList.tsx
+++ client/expense-tracker/src/components/ShowList.tsx
@@ -6,9 +6,9 @@ import { pushDataFromUser } from "../services/menu";
 export default function ShowData(){
     const [items, setItems] = useState<IDataList[]>([]);
     const [error, setError] = useState<Error |null>(null);
-    const [summ, setSum] = useState<number|null>(0);
-    const [rahulSpent, setRahulSpent] = useState<number|null>(0);
-    const [rameshSpent, setRameshSpent] = useState<number|null>(0);
+    const [sum, setSum] = useState<number|null>(0);
+    const [rahulSpent, setRahulSpent] = useState<number>(0);
+    const [rameshSpent, setRameshSpent] = useState<number>(0);
 
     useEffect(()=>{
         const fetchMenu = async () => {
@@ -16,6 +16,9 @@ export default function ShowData(){
                 const data = await getDataFromServer();
                 console.log(data);
                 setItems(data);
+                setSum(data.reduce((result,v) => result + v.price , 0))
+                calculateShares(data);
+
                 // let newPurchase = {
                 //     "payeeName": "Rahul",
                 //     "product": "water",
@@ -30,5 +33,55 @@ export default function ShowData(){
         }
         fetchMenu()
     },[]);
-    return (<h1>Show data comp</h1>);
+
+const calculateShares = (data: IDataList[]) => {
+    var rahulSpent1 : number = 0
+    var rameshSpent1 : number = 0
+    data.map(
+        sams => (
+            sams.payeeName === "Rahul" ? (
+                rahulSpent1 = rahulSpent1 + sams.price
+            ):
+            (
+                rameshSpent1 = rameshSpent1 + sams.price
+            )
+        )
+    )
+    setRahulSpent(rahulSpent1)
+    setRameshSpent(rameshSpent1)
+}
+
+    //return (<h1>Show data comp</h1>);
+    return (
+        <>
+            <header id="page-Header">Expense Tracker</header>
+
+            <>
+                <div className="use-inline date header-color">Date</div>
+                <div className="use-inline header-color">Product Purchased</div>
+                <div className="use-inline price header-color">Price</div>
+                <div className="use-inline header-color" style={{width: 112}}>Payee</div>
+            </>
+            {
+                items && items.map((user,idx)=>{
+                    return(<div key={idx}>
+                        <div className="use-inline date">{user.setDate}</div>
+                        <div className="use-inline">{user.product}</div>
+                        <div className="use-inline price">{user.price}</div>
+                        <div className={`use-inline ${user.payeeName}`}>{user.payeeName}</div>
+                    </div>)
+                })
+            }
+        <hr/>
+        <div className="use-inline">Total:</div>
+            <span className="use-inline total">{sum}</span> <br />
+            <div className="use-inline ">Rahul paid: </div>
+            <span className="use-inline total Rahul">{rahulSpent}</span> <br />
+            <div className="use-inline ">Ramesh paid: </div>
+            <span className="use-inline total Ramesh">{rameshSpent}</span> <br />
+            <span className="use-inline payable">{rahulSpent>rameshSpent? "Pay Rahul" : "Pay Ramesh"}</span> <br />
+            <span className="use-inline payable price">{Math.abs((rahulSpent-rameshSpent)/2)}</span> <br />
+
+        </>
+    )
 }
